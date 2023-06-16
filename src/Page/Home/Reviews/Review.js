f3dc172689a3ccedfbe2afc1cf526ff5e9332f34
rahulhasan31 <rahulhasan666@gmail.com>
2023-06-16 23:20:01 +0600

    added

--- src/Page/Home/Reviews/Review.js
+++ src/Page/Home/Reviews/Review.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Review = ({ reviews }) => {
-<<<<<<< HEAD
   const { name, img, review, location } = reviews;
   return (
     <div>
@@ -17,28 +16,6 @@ const Review = ({ reviews }) => {
               <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                 <img alt="" src={img} />
               </div>
-=======
-    const { name, img, review, location } = reviews
-    return (
-        <div>
-            <div className="card  shadow-xl border-t-2 border-l-2 border-red-500 hover:bg-teal-100" data-aos="fade-up"
-                data-aos-anchor-placement="top-bottom">
-                <div className="card-body">
-                    <p>{review}</p>
-                    <div className="flex items-center mt-6">
-                        <div className="avatar mr-6">
-                            <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                <img alt='' src={img} />
-                            </div>
-
-                        </div>
-                        <div>
-                            <h5 className='text-xl'>{name}</h5>
-                            <p>{location}</p>
-                        </div>
-                    </div>
-                </div>
->>>>>>> a2a6f40bc045b78aa9998a68c07ca4c4f5ae1cc6
             </div>
             <div>
               <h5 className="text-xl">{name}</h5>
